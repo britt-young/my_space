557c6f5d79b536e0bbf98edcf26737859794af96
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-12 12:09:35 -0500

    add
    asset images

--- my_space/src/Components/Footer.jsx
+++ my_space/src/Components/Footer.jsx
@@ -1,29 +1,45 @@
 import React from "react";
 
+const styles = {
+    footerStyles:{
+        minHeight: '100px',
+        background: '#595959',
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    imgStyles:{
+        width: '50px',
+        height: '50px'
+    }
+}
+
 const Footer = () => {
   return (
     <footer style={styles.footerStyles} className="p-3 mt-5">
       <a
         href="https://github.com/britt-young"
         className="m-3"
+        style={styles.imgStyles}
       >
-        <img src="your_image_url" alt="GitHub Icon" />;
+        <img src="./assets/github.png" alt="GitHub Icon" />;
       </a>
       <a
         href="https://www.linkedin.com/in/brittney-young-a1962a228/"
         className="m-3"
+        style={styles.imgStyles}
       >
-        <img src="your_image_url" alt="LinkedIn Icon" />;
+        <img src="./assets/linkedin.png" alt="LinkedIn Icon" />;
       </a>
       <a
         href="https://stackoverflow.com/"
         className="m-3"
+        style={styles.imgStyles}
       >
-        <img src="your_image_url" alt="StackOverflow Icon" />;
+        <img src="./assets/stack-overflow.png" alt="StackOverflow Icon" />;
       </a>
     </footer>
   );
-};
+}
 
 export default Footer;
 
