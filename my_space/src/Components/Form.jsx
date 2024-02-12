fde74b1053f744a75f64084c0c2446659c639282
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-12 13:08:40 -0500

    add
    contact form with helper function

---
+++ my_space/src/Components/Form.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { useState } from 'react';
+import { Row } from 'react-bootstrap';
+
+// Helper function to check if the email is valid
+import { validateEmail } from '../utils/helpers';
+
+const ContactForm = () => {
+  // Create state variables for input field and initialize them to empty strings
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Get the value and name of user input that triggered change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Set state to email, name, or none based on the input type
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (page refresh)
+    e.preventDefault();
+
+    // Check if the input is empty and/or valid. If not, set an error message
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid');
+      // Return to stop the code from running
+      return;
+        }
+
+    // Clear fields after form is submitted
+    setName('');
+    setEmail('');
+  };
+
+  return (
+    <div className="container">
+      <form className="form" onSubmit={handleFormSubmit}>
+        <Row>
+          {/* <label htmlFor="emailInput" className='form-label p-0'>Enter your email address</label> */}
+          <input 
+            className='form-control mb-3'
+            id='emailInput'
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="Email"
+          />
+        </Row>
+        <Row>
+          {/* <label htmlFor="nameInput" className='form-label p-0'>Enter First & Last Name</label> */}
+          <input
+            className='form-control mb-3'
+            id='nameInput'
+            value={name}
+            name="name"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="First & Last Name"
+          />
+        </Row>
+        <Row>
+          <a href='mailto:brittyoung_treece@protonmail.com' className='p-0'>
+            <button className='m-0' type="submit">Submit</button>
+          </a>
+        </Row>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="alert alert-danger my-3">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ContactForm;
---
+++ my_space/src/utils/helpers.js
@@ -0,0 +1,5 @@
+// Function that compares the entered email to a regex to make sure it's an email
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
