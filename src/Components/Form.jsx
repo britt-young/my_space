3d79521b4bcf40cf4efddf77d92d39ec8f48c7a7
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-14 11:52:19 -0500

    add
    form styling

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -21,22 +21,22 @@ const Contact = () => {
               <Container className='mt-5'>
                   <form id="contact-form" onSubmit={sendEmail}>
                       <div className="col-md-4 mb-3">
-                          <label htmlFor="validationDefault01">Your Name</label>
-                          <input type="text" className="form-control" id="validationDefault01" placeholder="Your name" required name="from_name" />
+                          <label htmlFor="validationDefault01">Name: </label>
+                          <input type="text" className="form-control" id="validationDefault01" placeholder="First & Last" required name="from_name" />
                       </div>
 
                       <div className="col-md-4 mb-3">
-                          <label htmlFor="validationDefault02">Email Address</label>
-                          <input type="email" className="form-control" id="validationDefault02" placeholder="Enter email" required name="email_from" />
+                          <label htmlFor="validationDefault02">Emai: </label>
+                          <input type="email" className="form-control" id="validationDefault02" placeholder="Email" required name="email_from" />
                       </div>
                       <div className="col-md-4 mb-3">
-                          <label htmlFor="validationDefault03">Subject</label>
-                          <input type="text" className="form-control" id="validationDefault03" placeholder="Subject" name="subject" />
+                          <label htmlFor="validationDefault03">Subject: </label>
+                          <input type="text" className="form-control" id="validationDefault03" placeholder="Subject " name="subject" />
                       </div>
 
                       <div className="col-md-4 mb-3">
-                          <label htmlFor="validationDefault04">Your Message</label>
-                          <textarea type="text-area" className="form-control" id="validationDefault04" placeholder="Your Message" required name="message" />
+                          <label htmlFor="validationDefault04">Your Message: </label>
+                          <textarea type="text-area" className="form-control" id="validationDefault04" placeholder="Type your message here" required name="message" />
                       </div>
                       <Button variant="light" type="submit">Submit</Button>
                   </form>
