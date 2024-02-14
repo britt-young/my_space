3551f8c547043b9b1225a016d999766471a5f82a
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-14 15:00:36 -0500

    refactor
    pages and component links

---
+++ src/Components/About.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "../Components/Styles/Home.css";
+import { useNavigate } from "react-router-dom";
+
+const styles = {
+  portraitStyle: {
+    margin: "5px",
+    maxWidth: "350px",
+    borderRadius: "0%",
+    boxShadow: "0 0 10px 5px rgba(0, 0, 0, 0.2)",
+  },
+};
+
+const About = () => {
+    const navigate = useNavigate();
+    const handleButtonClick = () => {
+      navigate("/contact");
+    };
+  
+    return (
+      <Container className="custom-container" fluid>
+        <Row className="custom-row">
+          <Col className="custom-col">
+            <h1>Hi, I'm Brittney!</h1>
+            <img
+              style={styles.portraitStyle}
+              className="portrait"
+              src="src\assets\britt.png"
+              alt="Brittney Treece"
+            />
+            <Button className="custom-btn" onClick={handleButtonClick}>
+              SEND ME A MESSAGE
+            </Button>
+          </Col>
+          <Col className="custom-col">
+            <h2>Full Stack Web Developer.</h2>
+            <p>
+              Recent graduate of The University of Utah Full Stack Coding
+              Bootcamp. I am focused on guiding small business owners into the
+              digital space by creating sales-driven websites that embody their
+              brand values and resonate with their target audience{" "}
+            </p>
+          </Col>
+          <Col className="custom-col"></Col>
+        </Row>
+      </Container>
+    );
+  };
+
+export default About
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,50 +1,15 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
-import "../Components/Styles/Home.css";
-import {  useNavigate  } from "react-router-dom";
+import About from "../Components/About";
+import Footer from "../Components/Footer";
 
-const styles = {
-  portraitStyle: {
-    maxWidth: "350px",
-  },
-};
-
-const About = () => {
-  const navigate = useNavigate();
-  const handleButtonClick = () => {
-navigate("/contact");
-  };
-
+const Home = () => {
   return (
-    <Container className='custom-container' fluid>
-      <Row className='custom-row'>
-        <Col className='custom-col'>
-          <h1>Hi, I'm</h1>
-          <h1>Brittney!</h1>
-          <Button className='custom-btn'onClick={handleButtonClick}>SEND ME A MESSAGE</Button>
-        </Col>
+    <div>
+      <About/>
+      
+      <Footer/>
+    </div>
+  )
+}
 
-        <Col className='custom-col'>
-          {/* <img
-            style={styles.portraitStyle}
-            className="portrait"
-            src="src\assets\britt.png"
-            alt="Brittney Treece"
-          /> */}
-        </Col>
-
-        <Col className='custom-col'>
-          <h2>Full Stack Web Developer.</h2>
-          <p>
-            Recent graduate of The University of Utah Full Stack Coding
-            Bootcamp. I am focused on guiding small business owners into the
-            digital space by creating sales-driven websites that embody their
-            brand values and resonate with their target audience{" "}
-          </p>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
-
-export default About;
+export default Home
