b245bc02a55a89270090da9d87ba5f62a2bbd954
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-14 21:53:11 -0500

    add
    resume temp + styling

--- src/Components/Quals.jsx
+++ src/Components/Quals.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import './Styles/Quals.css';
 import resume from '../assets/resume.pdf';
+import Button from 'react-bootstrap/Button'
 
 const Quals = () => {
   return (
-    <Section>
-      <div>
-      <a href="{resume}" download="proposed_file_name">Download</a>
-      </div>
-    </Section>
-  );
+    <main>
+        <section id="resume">
+            <h1>Front-end Proficiencies</h1>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>Response Design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+
+            <h1>Back-end Proficiencies</h1>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+        </section>
+        <Button>Download my <a href={resume} download>resume</a></Button>
+    </main>
+);
 }
 
 export default Quals
